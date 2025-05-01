a1540c6a74e4466a618c7fd9a463f2923f1f3655
unknown <ameytonannavar27@gmail.com>
2025-05-01 18:47:48 +0530

    Done Scne

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -64,7 +64,7 @@ export default function Login() {
     if (dataAPI.success) {
       toast.success(dataAPI.message);
 
-      navigate("/settings");
+      navigate("/options-analysis");
       fetchUserDetails();
     }
 
